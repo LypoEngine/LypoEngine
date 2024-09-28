b1b71c714be055557d40884fd1d959856eb6e17e
GuillaumeIsCoding <115158922+GuillaumeIsCoding@users.noreply.github.com>
2024-09-28 10:01:51 -0400

    Update Making API More Flexible

--- HiveEngine/src/core/Profiling/profiler.h
+++ HiveEngine/src/core/Profiling/profiler.h
@@ -10,18 +10,65 @@
 namespace hive
 {
     #define FUNCTION_NAME __func__
+    
+    #ifndef DEFAULT_PORT
+        #define DEFAULT_PORT profiler::DEFAULT_PORT            
+    #endif 
 
-    #define INIT_PROFILING          \
-        EASY_PROFILER_ENABLE;       \
-        profiler::startListen();
+    enum BlockStatus : uint8_t 
+    {
+        OFF = 0,                                              // The block is OFF
+        ON  = 1,                                              // The block is ON (but if it's parent block is off recursively then this block will be off too)
+        FORCE_ON = ON | 2,                                    // The block is ALWAYS ON (even if it's parent has turned off all children)
+        OFF_RECURSIVE = 4,                                    // The block is OFF and all of it's children by call-stack are also OFF.
+        ON_WITHOUT_CHILDREN = ON | OFF_RECURSIVE,             // The block is ON but all of it's children are OFF.
+        FORCE_ON_WITHOUT_CHILDREN = FORCE_ON | OFF_RECURSIVE, // The block is ALWAYS ON but all of it's children are OFF.   
+    };
 
-    #define FUNCTION_PROFILING(...) EASY_BLOCK(FUNCTION_NAME, ## __VA_ARGS__);
-
-    #define BLOCK_PROFILING(name) EASY_BLOCK(name)
-
+    /**
+     * Enable Profiling Tools
+     */
+    #define ENABLE_PROFILING            EASY_PROFILER_ENABLE;
+    /**
+     * Disable Profiling Tools
+     */
+    #define DISABLE_PROFILING           EASY_PROFILER_DISABLE;
+    /**
+     * Initialization of Profiling Tools. Block will be send via network
+     * @param DEFAULT_PORT If not defined the value will be 28077
+     */
+    #define INIT_NETWORK_PROFILING  \
+        ENABLE_PROFILING;           \
+        profiler::startListen(DEFAULT_PORT);
+    /**
+     * Stops profiling via newtwok
+     */
+    #define STOP_NETWORK_PROFILING      profiler::stopListen();
+    /**
+     * Dumps block into a file
+     * @param filename name of the file where blocks will be save
+     */
+    #define DUMP_PROFILING(filename)    profiler::dumpBlocksToFile(filename);
+    /**
+     * 
+     * @param name Name of the profling block
+     * @param color Color of the profiling block
+     */
+    #define BLOCK_PROFILING(name, ...)  EASY_BLOCK(name, ## __VA_ARGS__)
+    /**
+     * 
+     * @param  color Color of the profiling block
+     */
+    #define FUNCTION_PROFILING(...)     BLOCK_PROFILING(FUNCTION_NAME, ## __VA_ARGS__);
+    /**
+     * 
+     */
+    #define MARKER_PROFILING(name, ...) EASY_EVENT(name, ## __VA_ARGS__);
+    /**
+     * 
+     */
     #define END_BLOCK_PROFILING EASY_END_BLOCK
 
-    #define DUMP_PROFILING(path) profiler::dumpBlocksToFile(path);
 
 }
 
